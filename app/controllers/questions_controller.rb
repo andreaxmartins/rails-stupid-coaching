03d85d389a93603304ec23487ba69c77c342961b
Andrea Martins <andreaxmartins@gmail.com>
2022-05-16 16:00:59 +0200

    Rails: Routing, Controllers & Views - Stupid Coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:answer].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif params[:answer] == 'I am going to work'
+      @answer = 'Great!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
